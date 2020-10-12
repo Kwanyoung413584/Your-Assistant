22adfa97f2b7e692c3777e2f0a479eaa49bb98e8
Kwanyoung Christopher Kim <kwanyoung.kim@patelhs.org>
2020-10-12 16:53:28 +0000

    Fixed/made changes to the code, added a task command. And finished the assistant.

--- Main.java
+++ Main.java
@@ -1,17 +1,40 @@
+// Imports
 import java.util.Scanner;
+
+// Description:
+
+/* 
+This is an complicated code made by Kwanyoung Kim on 10/12/2020, 12:49 AM. 
+It is about a robot assistant that can do tasks, calculations, etc.
+*/
+
+
+
+
+
+
+
+
+
+
+
 
 class Main {
   public static void main(String[] args) {
-    Scanner GetScanner = new Scanner(System.in);
 
+   // Setup Major Values
+   // Get the scanner
+   Scanner GetScanner = new Scanner(System.in);
+   boolean TurnedOff = false;
+   // Array for the list of tasks that will be stored
+   String[] TaskList = {"","","","",""};
 
   
-  
+   // Start the Assistant
    System.out.println("Hello, what can I help you with? Say help if you do not know how to use me.");
 
-  
+   // Wait for first response
    String NextLine = GetScanner.next();
-   boolean SaidBye = false;
 
 
 
@@ -20,19 +43,86 @@ class Main {
 
 
 
+  // Start a infinite loop of communication until shutdown
+while(true){
 
-    while(true){
+  // Add a switch statement (I know how to use it since I can code java/javascript) This is a effective way to make commands instead of using if statement.
+  switch(NextLine.toLowerCase()){ //switch(given value) (The given value is what you put in the scanner.)
 
-    switch(NextLine.toLowerCase()){
-
+    // Cases are the values to compare with the given value.
     case "hello":
-      System.out.println("Hello");
+      System.out.println("Hello :)");
       break;
+
+    case "tasks":
+      if (TaskList[0] == "" && TaskList[1] == "" && TaskList[2] == "" && TaskList[3] == "" && TaskList[4] == "") {
+        System.out.println("You do not have any tasks yet! Add a task by typing in addtask");
+      }
+      else{
+        System.out.println("Here are your tasks:");
+        // Goes through the array and print it's contents
+        for (int i = 0; i < TaskList.length; i++){
+          System.out.println("[" + i + "] " + TaskList[i]);
+        }
+      }
+      break;
+
+    case "addtask":
+      System.out.println("Where do you want to put your new task? Putting it on a existing task location will overwrite it! Accepts locations between 0 - 4");
+      // Prevents the code from turning off when it gets a error. Possibly due to someone putting a double value or a string as the location of the task.
+      try{
+      int GetLocation = GetScanner.nextInt();
+      if (GetLocation > TaskList.length){
+        System.out.println("You cannot add/overwrite a task in a location above 4.");
+        break;
+      }
+      else if (GetLocation < 0){
+        System.out.println("You cannot add/overwrite a task in a location below 0.");
+        break;
+      }
+      System.out.println("What will be your content?");
+      String GetContent = GetScanner.next() + GetScanner.nextLine();
+
+      TaskList[GetLocation] = GetContent;
+      System.out.println("Successfully added/overwritted '" + GetContent + "' into task list " + GetLocation);
+      }
+      // This bottom code executes when it catches a error.
+      catch (Exception e){
+        System.out.println("That is not a valid input.");
+        break;
+      }
+      break;  
+
+    case "removetask":
+      System.out.println("What task do you want to remove? Accepts locations between 0 - 4");
+      // Prevents the code from turning off when it gets a error. Possibly due to someone putting a double value or a string as the location of the task.
+      try{
+      int GetLocation = GetScanner.nextInt();
+      if (GetLocation > TaskList.length){
+        System.out.println("You cannot remove a task in a location above 4.");
+        break;
+      }
+      else if (GetLocation < 0){
+        System.out.println("You cannot remove a task in a location below 0.");
+        break;
+      }
+      String OldTask = TaskList[GetLocation];
+      TaskList[GetLocation] = "";
+      System.out.println("Successfully removed '" + OldTask + "' from task list " + GetLocation);
+      }
+      // This bottom code executes when it catches a error.
+      catch (Exception e){
+        System.out.println("That is not a valid input.");
+        break;
+      }
+      break;  
 
     case "guessgame":
       System.out.println("Guess this number between 1 - 100, Type in 0 to cancel the game.");
       int GetGuess = GetScanner.nextInt(); 
+      // Randomizer from 1 - 100 (Has to be double, but I can convert it to int once it prints the correct number)
       double CorrectGuess = Math.floor((Math.random()*100)+1);
+
       while(GetGuess != CorrectGuess){
 
         if (GetGuess == 0){
@@ -67,6 +157,7 @@ class Main {
       int Oper;
       System.out.println("What is your operator [1 = add, 2 = subtract, 3 = multiply, 4 = divide, 5 = modulus");
       Oper = GetScanner.nextInt();
+      // Using || which means or in a code
       if (Oper == 1 || Oper == 2 || Oper == 3 || Oper == 4|| Oper == 5){
       if (Oper == 1){
       System.out.println("Result: " + (Num1 + Num2));
@@ -93,53 +184,55 @@ class Main {
       System.out.println("[Here are the list of commands:]");
       System.out.println("[calculate]");
       System.out.println("[hello]");
-      System.out.println("[bye]");
+      System.out.println("[shutdown]");
       System.out.println("[guessgame]");
+      System.out.println("[tasks]");
+      System.out.println("[addtask]");
+      System.out.println("[removetask]");   
       break;
 
-    case "bye":
-      SaidBye = true;
-      System.out.println("Goodbye");
+    case "shutdown":
+      System.out.println("Are you sure you want to shutdown? This will clear every data including tasks. This is irreversible! (true, false)");
+
+      // try catch will handle errors and prevent the code from shutting down
+      try {
+      boolean yesno = GetScanner.nextBoolean();
+
+      if (yesno == true){
+      TurnedOff = true;
+      System.out.println("Shutting down and clearing data...");
+      System.out.println("Assistant is successfully shutdown.");
+      }
+
+      else{
+      TurnedOff = false;
+      System.out.println("Canceled shutdown.");
+      }
+
+      }
+      catch (Exception e) {
+      TurnedOff = false;
+        System.out.println("That is a invalid input. Has to be a boolean. Canceled Shutdown.");
+      }
       break;
 
+    // When every case doesn't match with the given value, it will execute the script in default. This is like else in a if statement.
     default:
-      System.out.println("That is not a valid command");
+      //System.out.println("That is not a valid command");
+  }
 
-    }
-
-    NextLine = GetScanner.next();
-    if (SaidBye == true){
+    // Breaks the code and closes the scanner when value TurnedOff is set to true. The only way to set that value to true is when you do the shutdown command.
+    if (TurnedOff == true){
+      // Close the scanner
+      GetScanner.close();
       break;
     }
-    }
+    // Else tries to get the next line of the scanner.
+    else {
+      NextLine = GetScanner.next();
+    }   
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
   }
 }
